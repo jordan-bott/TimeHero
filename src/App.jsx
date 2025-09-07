f9688accaeda379bdc895316ec61596d7e1cddc9
jordanbott <jordanbott.dev@gmail.com>
2025-09-07 00:05:14 -0700

    change to manual time calc

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,10 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   const [currentHours, setCurrentHours] = useState(0)
-  const [goalHours, setGoalHours] = useState(40)
+  const [currentMins, setCurrentMins] = useState(0)
+  const [targetHours, setTargetHours] = useState(40)
   const [clockedInTime, setClockedInTime] = useState("12:00")
-  const [clockOutTime, setClockOutTime] = useState("12:00")
+  const [clockOutTime, setClockOutTime] = useState("")
 
   const decimalDict = {
     .01: 1,
@@ -19,30 +20,20 @@ function App() {
     .10: 6,
   }
 
-  function calcClockOut() {
-    var neededHours = goalHours - currentHours
-    var decimal = neededHours % 1
-    var twoDecimals = Math.round(decimal * 100) / 100
-    var numOfMins = decimalDict[twoDecimals]
-    var numOfHours = Math.floor(neededHours)
-    var hoursToMins = numOfHours * 60
-    var totalNumMins = numOfMins + hoursToMins
-    // need to set the clocked in time to a Date value and then can do set time with the minutes
-    var clockedInDate = new Date(`2024-08-08:${clockedInTime}`)
-    var clockedInDateWTime = clockedInDate.setTime(clockedInDate.getTime() + (totalNumMins * 60 * 1000))
-    setClockOutTime(clockedInDateWTime)
-  }
-
-  console.log(clockedInTime)
 
   const handleCurrentHourChange = (event) => {
     const value = event.target.value;
     setCurrentHours(value);
   };
 
-  const handleGoalHourChange = (event) => {
+  const handleCurrentMinChange = (event) => {
     const value = event.target.value;
-    setGoalHours(value);
+    setCurrentMins(value);
+  };
+
+  const handleTargetHourChange = (event) => {
+    const value = event.target.value;
+    setTargetHours(value);
   };
 
   const handleClockedInTimeChange = (event) => {
@@ -50,16 +41,36 @@ function App() {
     setClockedInTime(value);
   };
 
+  const calculateClockOut = () => {
+    if (currentMins === 0) {
+      var hoursTilClockOut = targetHours - currentHours
+      var minsTilClockOut = 0
+    } else {
+      var hoursTilClockOut = targetHours - currentHours - 1
+      var minsTilClockOut = 60 - currentMins
+    }
+    var clockOutHour = clockedInTime.substring(0,2) + hoursTilClockOut
+    var clockOutMin = clockedInTime.substring(3, 5) + minsTilClockOut
+
+    setClockOutTime(`${clockOutHour}:${clockOutMin}`)
+  }
+
+console.log(clockOutTime)
+
   return (
     <>
-      <form onSubmit={calcClockOut}>
+      <form onSubmit={calculateClockOut}>
+        <label htmlFor="Current Hours">Current Total Hours</label>
         <input onChange={handleCurrentHourChange} value={currentHours} type="number" name="Current Hours"/>
-        <input onChange={handleGoalHourChange} value={goalHours} type="number" name="Goal Hours"/>
-        <input onChange={handleClockedInTimeChange} value={clockedInTime} type="time" />
-        <button type="submit" >Submit</button>
-
-      <p>Time to clock out: {clockOutTime}</p>
+        <label htmlFor="Current Minutes">Current Total Minute Decimal</label>
+        <input onChange={handleCurrentMinChange} value={currentMins} type="number" name="Current Minutes" pattern="\d"/>
+        <label htmlFor="Target Hours">Target Hours</label>
+        <input onChange={handleTargetHourChange} value={targetHours} type="number" name="Target Hours"/>
+        <label htmlFor="Clocked In Time">Clock In Time</label>
+        <input onChange={handleClockedInTimeChange} value={clockedInTime} type="time" name="Clocked In Time"/>
+        <button type="submit" >Calculate Clock Out Time</button>
       </form>
+      <p>Time to clock out: {clockOutTime}</p>
     </>
   )
 }
